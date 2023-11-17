87c864283bec7f513138a03b50631d9e4ab01405
Mike Collins <fixitman.mike@outlook.com>
2023-11-17 16:43:42 -0500

    peersist auth state after refresh

--- RadzenUI/Data/AppState.cs
+++ RadzenUI/Data/AppState.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using RadzenUI.Pages.Auth.Models;
 
 
@@ -7,18 +8,44 @@ namespace RadzenUI.Data;
 
 public class AppState
 {
+	private static readonly string APPUSER = "AppUser";
+	
 	private AppUser? user;
 
-	public AppUser? User { 
+	public ProtectedSessionStorage? Session { get; set; } = null;
+
+    public AppUser? User { 
 		get => user; 
 		set
 		{
 			user = value;
-			Persist();
+			Save();	
 		}  
 	}
 
+	public async Task Load()
+	{
+		if(Session != null)
+		{
+			var r = await Session.GetAsync<AppUser>(APPUSER);
+			if(r.Success) user = r.Value;
+		}
+	}
 
+    public async Task Save()
+    {
+       if(Session != null && User != null)
+		{
+			await Session.SetAsync(APPUSER,User);
+		}
+        
+    }
+
+	public async Task InitializeAsync(ProtectedSessionStorage session)
+	{
+		Session = session;
+		await Load();
+	}
 
 }
 
--- RadzenUI/Data/User.cs
+++ RadzenUI/Data/User.cs
@@ -6,10 +6,7 @@ public class User
     public string Username { get; set; }
     public string Email { get; set; }
     public string PWHash { get; set; }
-    public string LastCalendar { get; set; } = "";
-
-
-
-    
+    public string LastCalendar { get; set; } = "";   
+    public string AppRole { get; set; }
     
 }
